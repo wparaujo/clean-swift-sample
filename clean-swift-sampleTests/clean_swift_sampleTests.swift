ea8ea7963ebe67af2f1ceb623e3c27b3d0b4edb9
Wesley Araujo <wparaujo@gmail.com>
2020-01-13 16:00:40 -0300

    Initial Commit

---
+++ clean-swift-sampleTests/clean_swift_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  clean_swift_sampleTests.swift
+//  clean-swift-sampleTests
+//
+//  Created by mac on 13/01/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import XCTest
+@testable import clean_swift_sample
+
+class clean_swift_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
